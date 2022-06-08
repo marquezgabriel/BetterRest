17329da2f798c69dd5659a9dc7a8fa6ce19e414c
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-08 10:25:13 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Gabriel Marquez on 2022-06-08.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
